8af0c81526f526b4df1c058c2699e32ad7114388
RaedGhodhbane <ghodhbane.raed@gmail.com>
2022-01-30 19:50:09 +0100

    Front-end commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <header>
+      <div class="title">
+        <h1>IT jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+
+    <JobsList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
+import JobsList from "./components/JobsList.vue";
 import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobsList },
+
   setup() {
     // const state = reactive({
     //   name : 'Link',
@@ -36,15 +50,61 @@ export default defineComponent({
         id: "1",
       },
       {
-        title: "farm worker",
-        location: "lon lon ranch",
-        salary: 30000,
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
         id: "2",
       },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      {
+        title: "fisherman",
+        location: "lake hylia",
+        salary: 21000,
+        id: "4",
+      },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
     ]);
-    return { jobs };
+    const order = ref<OrderTerm>("title");
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header h1 {
+  font-size: 3em;
+}
+</style>
